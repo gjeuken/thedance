6daa3dc7207c3ba906030e477747d52efecd052a
Gustavo Jeuken <gjeuken@gmail.com>
2021-03-07 14:49:36 +0100

    start with full hands

--- src/Game.js
+++ src/Game.js
@@ -6,6 +6,12 @@ function CreateDeck(start, end) {
 	return deck.map((a) => ({sort: Math.random(), value: a})).sort((a, b) => a.sort - b.sort).map((a) => a.value)
 } // returns a shuffled deck
 
+function NumberOfCards(n_players) {
+	if (n_players === 1) { return 8 }
+	else if (n_players === 2) { return 7 }
+	else { return 6 }
+}
+
 function DrawCard(G, ctx) {
     while (G.hand[ctx.currentPlayer].length < G.hand_size && G.deck.length > 0) {
         const card = G.deck.pop();
@@ -27,12 +33,6 @@ function PlayCard(G, ctx, card, pile_id) {
     G.hand[ctx.currentPlayer].splice(card_idx, 1)
 }
 
-function NumberOfCards(n_players) {
-	if (n_players === 1) { return 8 }
-	else if (n_players === 2) { return 7 }
-	else { return 6 }
-}
-
 function UpdateScore(G, ctx) {  // Not using anywhere yet. Not tested.
     var score = G.length
     var  n;
@@ -43,13 +43,19 @@ function UpdateScore(G, ctx) {  // Not using anywhere yet. Not tested.
 }
 
 export const TheDance = {
-	setup: ctx => ({
-		piles: [1, 1, 100, 100],
-		deck: CreateDeck(2, 99),
-		hand_size: NumberOfCards(ctx.numPlayers),
-		hand: Array(ctx.numPlayers).fill([]),
-		score: 98,
-	}),
+	setup: ctx => {
+		let deck = CreateDeck(2,99);
+		let hand_size = NumberOfCards(ctx.numPlayers)
+		let hand = Array(ctx.numPlayers).fill([]);
+		for (let i = 0; i < ctx.numPlayers; i++) {
+			hand[i] = deck.splice(0, hand_size);
+	}
+		return ({ piles: [1, 1, 100, 100],
+		deck: deck,
+		hand_size: hand_size,
+		hand: hand,
+		score: 98,})
+	},
 
 	phases: {
 	    main_phase: {
